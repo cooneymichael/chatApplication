7d0a0b0101cf67ea9c9e825d858de77d98bd948f
Michael Cooney <mcooney296@gmail.com>
2018-10-27 20:44:45 -0400

    began filling out methods for ncursesUI.h, used cursesGui.cpp as testing file

--- cursesGui.cpp
+++ cursesGui.cpp
@@ -1,25 +1,40 @@
-#include <ncurses.h>
+#include <curses.h>
+#include <list>
+#include <string>
 #include <iostream>
+
+//don't forget to link -lncurses
 
 int main(){
 
-  initscr();
-  if (!has_colors() && !can_change_color()){
-      endwin();
-      std::cout << "Terminal does not support colors" << std::endl;
-    }
+  // initscr();
+  // cbreak();
+  // if (!has_colors() && !can_change_color()){
+  //     endwin();
+  //     std::cout << "Terminal does not support colors" << std::endl;
+  //   }
 
-  //write a string to screen; white foreground, red background
+  // //write a string to screen; white foreground, red background
 
-  start_color();
-  init_pair(1, COLOR_WHITE, COLOR_RED);
-  attron(COLOR_PAIR(1));
+  // start_color();
+  // init_pair(1, COLOR_WHITE, COLOR_RED);
+  // attron(COLOR_PAIR(1));
 
-  addstr("Hello World");
-  refresh();
-  getch();
+  // WINDOW* myWin = newwin(20, 10, 6, 6);
+  // box(myWin, '#', 0);
+  
+  // waddstr(myWin, "Hello World, I am a string longer than 10 characters :)");
+  // wrefresh(myWin);
+  // wgetch(myWin);
 
-  endwin();
+  // endwin();
+  
+  
+
+  //for(int i=0; i<j, i++){
+    
+  //  }
+  
 
   return 0;
 
--- ncursesUI.h
+++ ncursesUI.h
@@ -16,13 +16,41 @@ public:
     //erase the screen when done
   }
 
-  void CreateWindow(){
+  WINDOW* CreateWindow(int height, int width, int yStart, int xStart,\
+		       char vertChar, char horizChar){
     //create a window using the ncurses api
+    WINDOW* win = newwin(height, width, yStart, xStart);
+    box(win, vertChar, horizChar);
+    wrefresh(win);
+    return win;
+
   }
 
   void AddMessage(std::tuple<std::string, std::string> messageAndUser, \
-			    WINDOW *win) {
-    //use waddwstr to print message from a user 
+		  int iterator, WINDOW *win) {
+    //use waddwstr to print message from a user
+    //TODO: unpack tuple, mv to window, mv cursor to correct position, rename vars
+
+    std::string x2 = x.substr(0, iterator);
+    std::cout << "\"" << x <<"\"" << " ::: " << "\"" << x2 << "\"" << std::endl;
+    
+    std::list<std::string> messageList;
+    messageList.push_back(x2);
+    x.erase(0, iterator);
+    std::cout << "\"" << x <<"\"" << " ::: " << "\"" << x2 << "\"" << std::endl;
+    
+    // for addMessage
+    //   -> need a way of keeping track of where to place next message in window
+    // 	   -> how?
+    // 	   -> go n chars into string, then backtrack to first space found
+    // 	   -> create a substring from that point forward (while str.length !> n)
+    // 	   -> assign to variable, assign all substrings to a vector (?)
+    // 	   -> print each vector element to screen
+	   
+    // 	   -> cut string from 0 to n chars in
+    // 	   -> add that portion to vector
+
+
   }
   
   void UpdateUsers(){
